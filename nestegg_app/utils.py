65f8de484af251860622900daa11c50bd14207a6
Gerry <jg.villoslado@gmail.com>
2024-11-03 21:11:39 -0800

    Worked on utils.py

--- nestegg_app/utils.py
+++ nestegg_app/utils.py
@@ -1,3 +1,7 @@
+"""
+Import modules and define functions to load data and get user input
+"""
+
 import os
 import sys
 import random
@@ -22,15 +26,60 @@ def default_input(prompt, default=None):
         return response
 
 
+"""
+Load data files with original data in percent form
+"""
 print("\nNote: Input data should be in percent format (e.g. 5.0 for 5%)")
 
 try:
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    bonds = read_to_list(os.path.join(base_dir, "data", "10-yr_TBond_returns_1926-2013_pct.txt"),)
-    stocks = read_to_list(os.path.join(base_dir, "data", "SP500_returns_1926-2013_pct.txt"),)
-    blend_40_50_10 = read_to_list(os.path.join(base_dir, "data", "40-50-10_returns_1926-2013_pct.txt"),)
-    blend_50_50 = read_to_list(os.path.join(base_dir, "data", "50-50_returns_1926-2013_pct.txt"),)
-    infl_rate = read_to_list(os.path.join(base_dir, "data", "inflation_rate_1926-2013_pct.txt"),)
+    bonds = read_to_list(
+        os.path.join(base_dir, "data", "10-yr_TBond_returns_1926-2013_pct.txt"),
+    )
+    stocks = read_to_list(
+        os.path.join(base_dir, "data", "SP500_returns_1926-2013_pct.txt"),
+    )
+    blend_40_50_10 = read_to_list(
+        os.path.join(base_dir, "data", "40-50-10_returns_1926-2013_pct.txt"),
+    )
+    blend_50_50 = read_to_list(
+        os.path.join(base_dir, "data", "50-50_returns_1926-2013_pct.txt"),
+    )
+    infl_rate = read_to_list(
+        os.path.join(base_dir, "data", "inflation_rate_1926-2013_pct.txt"),
+    )
 except IOError as e:
     print("Error reading file: {}".format(e))
     sys.exit(1)
+
+"""
+Get user input; use dictionary for investment-type arguments
+Print input legend for user
+"""
+investment_type_args = {
+    "bonds": bonds,
+    "stocks": stocks,
+    "sb_blend": blend_50_50,
+    "sbc_blend": blend_40_50_10,
+}
+print("   stocks = S&P 500")
+print("   bonds = 10-year Treasury Bonds")
+print("   sb_blend = 50% stocks, 50% bonds")
+print("   sbc_blend = 40% stocks, 50% bonds, 10% cash\n")
+print("Press ENTER to take the default value in brackets\n")
+
+"""
+Get user input for investment type
+"""
+invest_type = default_input(
+    "Enter investment type: (stocks, bonds, sb_blend, sbc_blend: \n", "bonds"
+).lower()
+while invest_type not in investment_type_args:
+    invest_type = input("Invalid input. Enter investment type: ").lower()
+
+"""
+Get user input for starting value of investment
+"""
+start_value = default_input("Enter starting value of investment: \n", "10000")
+while not start_value.isdigit():
+    start_value = input("Invalid input. Enter starting value of investment: ")

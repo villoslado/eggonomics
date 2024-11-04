06e0bdd6bf6f7b21c211fc86a3259cd24db260f4
Gerry <jg.villoslado@gmail.com>
2024-11-03 19:38:32 -0800

    Working on utils.py

--- nestegg_app/utils.py
+++ nestegg_app/utils.py
@@ -1,3 +1,20 @@
 import sys
 import random
 import matplotlib.pyplot as plt
+
+
+def read_to_list(file_name):
+    # Open a file of data in percent, convert to decimal and return a list
+    with open(file_name) as in_file:
+        lines = [float(line.strip()) for line in in_file]
+        decimal = [round(line / 100, 5) for line in lines]
+        return decimal
+
+def default_input(prompt, default=None):
+    # Allow use of default values in input
+    prompt = '{} [{}]: '.format(prompt, default)
+    response = input(prompt)
+    if not response and default:
+        return default
+    else:
+        return response
